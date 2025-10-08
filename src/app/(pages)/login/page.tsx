4e38a9cfbe0acd9ab20c17821120567b3d92ee8a
Tấn Tài <tantailuong099@gmail.com>
2025-10-08 12:32:38 +0700

    git init

---
+++ src/app/(pages)/login/page.tsx
@@ -0,0 +1,10 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Trang Đăng Nhập",
+  description: "Tutor Support System",
+};
+
+export default function LoginPage() {
+  return <></>;
+}
